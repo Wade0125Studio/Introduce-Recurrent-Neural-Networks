587bcb3c71f9c59bc6b9167ccc4815d60d3d206b
wade0125 <100984053+wade0125@users.noreply.github.com>
2022-05-21 17:40:56 +0800

    2022/5/21

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Introduce-Recurrent-Neural-Networks
+
+
+
+
+<p align="center">Long short-term memory</font></p>
+
+
+<div align="center">
+<img src="https://github.com/wade0125/DeeplabV3-Plus-Tensorflow2.X/blob/main/miou_out/mIoU_mobilenetv2.png">
+</div>
  
---
+++ intro-to-recurrent-neural-networks-lstm-gru.py
@@ -0,0 +1,148 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=5120)])
+  except RuntimeError as e:
+    print(e)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, GRU, Bidirectional
+from tensorflow.keras.optimizers import SGD
+import math
+from sklearn.metrics import mean_squared_error
+
+# Some functions to help out with
+def plot_predictions(test,predicted):
+    plt.plot(test, color='red',label='Real IBM Stock Price')
+    plt.plot(predicted, color='blue',label='Predicted IBM Stock Price')
+    plt.title('IBM Stock Price Prediction MSE:3.359')
+    plt.xlabel('Time')
+    plt.ylabel('IBM Stock Price')
+    plt.legend()
+    plt.savefig("C:/Users/GIGABYTE/Downloads/intro-to-recurrent-neural-networks/img/IBM-Stock-Price-Prediction-GRU.png")
+    plt.show()
+
+def return_rmse(test,predicted):
+    rmse = math.sqrt(mean_squared_error(test, predicted))
+    print("The root mean squared error is {}.".format(rmse))
+
+# First, we get the data
+dataset = pd.read_csv('C:/Users/GIGABYTE/Downloads/intro-to-recurrent-neural-networks/DataBase/IBM_2006-01-01_to_2018-01-01.csv', index_col='Date', parse_dates=['Date'])
+dataset.head()
+
+
+# Checking for missing values
+training_set = dataset[:'2016'].iloc[:,1:2].values
+test_set = dataset['2017':].iloc[:,1:2].values
+
+
+dataset["High"][:'2016'].plot(figsize=(16,4),legend=True)
+dataset["High"]['2017':].plot(figsize=(16,4),legend=True)
+plt.legend(['Training set (Before 2017)','Test set (2017 and beyond)'])
+plt.title('IBM stock price')
+plt.savefig("C:/Users/GIGABYTE/Downloads/intro-to-recurrent-neural-networks/img/show dataset.png")
+plt.show()
+
+sc = MinMaxScaler(feature_range=(0,1))
+training_set_scaled = sc.fit_transform(training_set)
+
+X_train = []
+y_train = []
+for i in range(60,2769):
+    X_train.append(training_set_scaled[i-60:i,0])
+    y_train.append(training_set_scaled[i,0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_train = np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1))
+
+
+#--------------------------LSTM-----------------------------
+
+# The LSTM architecture
+regressor = Sequential()
+# First LSTM layer with Dropout regularisation
+regressor.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+# Second LSTM layer
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+# Third LSTM layer
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+# Fourth LSTM layer
+regressor.add(LSTM(units=50))
+regressor.add(Dropout(0.2))
+# The output layer
+regressor.add(Dense(units=1))
+# Compiling the RNN
+regressor.compile(optimizer='rmsprop',loss='mean_squared_error')
+# Fitting to the training set
+regressor.fit(X_train,y_train,epochs=50,batch_size=32)
+
+
+dataset_total = pd.concat((dataset["High"][:'2016'],dataset["High"]['2017':]),axis=0)
+inputs = dataset_total[len(dataset_total)-len(test_set) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs  = sc.transform(inputs)
+
+# Preparing X_test and predicting the prices
+X_test = []
+for i in range(60,311):
+    X_test.append(inputs[i-60:i,0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+
+plot_predictions(test_set,predicted_stock_price)
+return_rmse(test_set,predicted_stock_price)
+
+
+#--------------------------GRU-----------------------------
+
+# The GRU architecture
+regressorGRU = Sequential()
+# First GRU layer with Dropout regularisation
+regressorGRU.add(GRU(units=50, return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Second GRU layer
+regressorGRU.add(GRU(units=50, return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Third GRU layer
+regressorGRU.add(GRU(units=50, return_sequences=True, input_shape=(X_train.shape[1],1), activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# Fourth GRU layer
+regressorGRU.add(GRU(units=50, activation='tanh'))
+regressorGRU.add(Dropout(0.2))
+# The output layer
+regressorGRU.add(Dense(units=1))
+# Compiling the RNN
+regressorGRU.compile(optimizer=SGD(lr=0.01, decay=1e-7, momentum=0.9, nesterov=False),loss='mean_squared_error')
+# Fitting to the training set
+regressorGRU.fit(X_train,y_train,epochs=50,batch_size=150)
+
+
+X_test = []
+for i in range(60,311):
+    X_test.append(inputs[i-60:i,0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+GRU_predicted_stock_price = regressorGRU.predict(X_test)
+GRU_predicted_stock_price = sc.inverse_transform(GRU_predicted_stock_price)
+
+# Visualizing the results for GRU
+plot_predictions(test_set,GRU_predicted_stock_price)
+# Evaluating GRU
+return_rmse(test_set,GRU_predicted_stock_price)
+
+
+
+
+
+
